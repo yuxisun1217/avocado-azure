fbe15d0d85da4e7398347813521b757c93fdc5e1
Yuxin Sun <yuxisun@redhat.com>
2017-01-19 15:03:31 +0800

    Add Standard_A1_v2 and Standard_A8m_v2

--- tests/03_general.py
+++ tests/03_general.py
@@ -369,13 +369,13 @@ class GeneralTest(Test):
         error_log = ""
         warn_log = ""
         if self.azure_mode == "asm":
-            vm_size_list = ["Small", "A11", "Standard_D1", "Standard_D15_v2",
-                            "Standard_DS1", "Standard_DS15_v2", "Standard_G1", "Standard_GS5",
-                            "Standard_F1", "Standard_F16s"]
+            vm_size_list = ["Small", "A11", "Standard_A1_v2", "Standard_A8m_v2",
+                            "Standard_D1", "Standard_D15_v2", "Standard_DS1", "Standard_DS15_v2",
+                            "Standard_G1", "Standard_GS5", "Standard_F1", "Standard_F16s"]
         else:
-            vm_size_list = ["Standard_A1", "Standard_A11", "Standard_D1", "Standard_D15_v2",
-                            "Standard_DS1", "Standard_DS15_v2", "Standard_G1", "Standard_GS5",
-                            "Standard_F1", "Standard_F16s"]
+            vm_size_list = ["Standard_A1", "Standard_A11", "Standard_A1_v2", "Standard_A8m_v2",
+                            "Standard_D1", "Standard_D15_v2", "Standard_DS1", "Standard_DS15_v2",
+                            "Standard_G1", "Standard_GS5", "Standard_F1", "Standard_F16s"]
         for vm_size in vm_size_list:
             # Create VM
             vm_params = copy.deepcopy(self.vm_params)
